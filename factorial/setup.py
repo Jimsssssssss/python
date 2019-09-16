105fe7a932ec69ab599d9d3e7d9903967189a610
yanmuyuan <562985990@qq.com>
2019-09-16 09:55:07 +0800

    提交

---
+++ factorial/setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from setuptools import find_packages, setup
+setup(name='factorial',
+        version = '0.1',
+        description = "Factorial module.",
+        long_description = "A test module for our book.",
+        platforms = ["Linux"],
+        url="https://www.shiyanlou.com/courses/596",
+        license = "MIT",
+        packages=find_packages()
+        )
